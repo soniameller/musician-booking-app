1eaafd2adc9da6e7d96de2ff98ce75e8733cda60
Sonia Meller <mellersonia@gmail.com>
2024-02-12 10:39:02 +0000

    server/src/services/: added musician and schedule services

---
+++ server/src/services/musicianService.ts
@@ -0,0 +1,29 @@
+import Musician, { JsonMusician } from '../database/models/musician';
+import Schedule from '../database/models/schedule';
+import Service from '../database/models/service';
+
+export const fetchEnabledMusicians = async (): Promise<JsonMusician[]> => {
+  try {
+    const dbMusicians = await Musician.findAll({
+      where: { enabled: true },
+      include: [
+        {
+          model: Schedule,
+          attributes: ['dateTime'],
+        },
+        {
+          model: Service,
+          attributes: ['name'],
+        },
+      ],
+      attributes: {
+        exclude: ['createdAt', 'updatedAt'],
+      },
+    });
+
+    return dbMusicians.map((musician) => musician.customJson);
+  } catch (error) {
+    console.error('Error fetching musicians from the database:', error);
+    throw new Error('Failed to fetch musicians');
+  }
+};
