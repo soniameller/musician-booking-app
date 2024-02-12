815ae73ffe9eedbb348077cd000888a89b7252e7
Sonia Meller <mellersonia@gmail.com>
2024-02-12 10:42:16 +0000

    server/src/main.ts:  Initialize express JSON middleware and base API routes

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import { syncDatabase } from './database/sequelize';
+import routes from './routes/index';
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 const app = express();
+app.use(express.json());
+
+app.use('/api', routes);
 
 app.get('/', (req, res) => {
   res.send({ message: 'Hello API' });
