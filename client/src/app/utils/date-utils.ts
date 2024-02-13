b60b2b731103592405769c838ccfa8e69e3fe380
Sonia Meller <mellersonia@gmail.com>
2024-02-13 21:14:53 +0000

    client/src/app/utils/date-utils.ts: new file

---
+++ client/src/app/utils/date-utils.ts
@@ -0,0 +1,13 @@
+export const formatDateToTimeStr = (inputTimestamp: Date): string => {
+  const dateObject = new Date(inputTimestamp);
+  const formattedTime = dateObject.toLocaleTimeString([], {
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false,
+  });
+  return formattedTime;
+};
+
+export const isDateTimePast = (dateTime: Date) => {
+  return new Date(dateTime) < new Date();
+};
