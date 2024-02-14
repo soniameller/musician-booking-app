2d9e4e2bd700aad43a9bf477601a501e0199bc00
Sonia Meller <mellersonia@gmail.com>
2024-02-14 15:45:25 +0000

    server/src/: models, database, service & route refactor after adding bookings

--- server/src/services/scheduleService.ts
+++ server/src/services/scheduleService.ts
@@ -1,41 +1,39 @@
-import { FindOptions } from 'sequelize/types';
+import { Op } from 'sequelize';
 import Musician from '../database/models/musician';
-import MusicianService from '../database/models/musicianService';
 import Schedule from '../database/models/schedule';
 import Service from '../database/models/service';
-import { sequelize } from '../database/sequelize';
 import { JsonSchedule } from '@shared-utils';
-
-export interface BookingRequest {
-  name: string;
-  serviceId: string;
-}
-
-const scheduleQueryConfig: FindOptions<any> = {
-  include: [
-    {
-      model: Musician,
-      where: { enabled: true },
-      include: [
-        {
-          model: Service,
-          attributes: ['name', 'id'],
-        },
-      ],
-    },
-    {
-      model: Service,
-      attributes: ['name'],
-    },
-  ],
-};
+import Booking from '../database/models/booking';
+import { startOfDay } from 'date-fns';
 
 export const fetchMusicianSchedules = async (
   musicianId: string
 ): Promise<JsonSchedule[]> => {
+  const today = new Date();
+
   try {
     const dbSchedules = await Schedule.findAll({
-      ...scheduleQueryConfig,
+      include: [
+        {
+          model: Musician,
+          where: { enabled: true },
+          include: [
+            {
+              model: Service,
+              attributes: ['name', 'id'],
+            },
+          ],
+        },
+        {
+          model: Booking,
+          where: {
+            createdAt: {
+              [Op.gte]: startOfDay(today),
+            },
+          },
+          required: false,
+        },
+      ],
       where: { '$Musician.id$': musicianId },
     });
 
@@ -43,74 +41,5 @@ export const fetchMusicianSchedules = async (
   } catch (error) {
     console.error('Error fetching schedules from the database:', error);
     throw new Error('Failed to fetch schedules');
-  }
-};
-
-export const fetchBookedSchedules = async (): Promise<JsonSchedule[]> => {
-  try {
-    const dbSchedules = await Schedule.findAll({
-      ...scheduleQueryConfig,
-      where: { booked: true },
-      order: [['dateTime', 'ASC']],
-    });
-
-    return dbSchedules.map((schedule) => schedule.customJson);
-  } catch (error) {
-    console.error('Error fetching schedules from the database:', error);
-    throw new Error('Failed to fetch schedules');
-  }
-};
-
-export const bookSchedule = async (
-  scheduleId: string,
-  params: BookingRequest
-): Promise<JsonSchedule> => {
-  const { name, serviceId } = params;
-
-  const transaction = await sequelize.transaction();
-  try {
-    const schedule = await Schedule.findByPk(scheduleId, { transaction });
-
-    if (!schedule) {
-      throw new Error('Schedule not found');
-    }
-    if (schedule.booked) {
-      throw new Error('Schedule is already booked and cannot be updated.');
-    }
-
-    const isServiceOfferedByMusician = await MusicianService.findOne({
-      where: {
-        musicianId: schedule.musicianId,
-        serviceId,
-      },
-      transaction,
-    });
-
-    if (!isServiceOfferedByMusician) {
-      throw new Error('The selected service is not offered by the musician.');
-    }
-
-    await schedule.update(
-      {
-        name,
-        booked: true,
-        serviceId,
-      },
-      { transaction }
-    );
-
-    await transaction.commit();
-
-    const updatedSchedule = await Schedule.findByPk(
-      schedule.id,
-      scheduleQueryConfig
-    );
-
-    return updatedSchedule.customJson;
-  } catch (error) {
-    await transaction.rollback();
-
-    console.error('Error updating schedule:', error);
-    throw error;
   }
 };
