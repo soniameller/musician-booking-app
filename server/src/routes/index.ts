fd05cd8584969f3b56cd12feb9c4aaa79287181c
Sonia Meller <mellersonia@gmail.com>
2024-02-14 15:44:04 +0000

    server/src:server/src/: BOOKING routes, services, model & controller

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import musicianRoutes from './musicianRoutes';
 import schedulesRoutes from './scheduleRoutes';
+import bookingRoutes from './bookingRoutes';
 
 const router = express.Router();
 
 router.use('/musicians', musicianRoutes);
 router.use('/schedules', schedulesRoutes);
+router.use('/bookings', bookingRoutes);
 
 export default router;
